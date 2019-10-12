1ead357fd1f0fd7d22973fb60231068c31f333c9
Chris Tsang <419761217@qq.com>
2019-10-13 04:12:38 +0800

    init version
    init version

---
+++ springcloud/springcloud-infra-zuul/src/main/java/com/springcloud/infra/zuul/ZuulApplication.java
@@ -0,0 +1,20 @@
+package com.springcloud.infra.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author Chris Tsang
+ *
+ */
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
